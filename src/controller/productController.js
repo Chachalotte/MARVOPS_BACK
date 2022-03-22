e737e8353e6e134e85364a6561cc908083d370d3
Charlotte Kindt <37578645+Chachalotte@users.noreply.github.com>
2022-03-22 17:33:26 +0100

    Added find by id product

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -6,3 +6,32 @@ exports.insertProduct = (req, res, next) => {
     newProduct.save();
     res.status(200).send(newProduct);
 };
+
+exports.editAProduct = (req, res, next) => {
+    const modelId = req.params.idProduct;
+    const newName = req.body.name;
+
+    productModel.findById(modelId).then((model) => {
+        return Object.assign(model, {title: newName});
+    }).then((model) => {
+        return model.save();
+    }).then((updatedModel) => {
+        res.json({
+            msg: 'model updated',
+            updatedModel
+        });
+    }).catch((err) => {
+        res.send(err);
+    });
+}
+
+exports.getProductsFiltered = (req, res, next) => {
+    const filters = req.body.filters;
+
+    filters.forEach(element => {
+        console.log(element);
+        productModel.find({"category.type" : element }).then((model) => {
+            console.log('trouvé !');
+        });
+    })
+}
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 const { insertProduct } = require('../controller/productController')
+const { editAProduct } = require('../controller/productController')
+const { getProductsFiltered } = require('../controller/productController')
 
-router.post('/insert', insertProduct );
-
+router.put('/insert', insertProduct );
+router.post('/:idProduct/edit', editAProduct );
+router.get('/filtered', getProductsFiltered );
 
 module.exports = router
