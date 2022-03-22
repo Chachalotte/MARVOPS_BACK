7d1616582364dcd2e4326c6f4824f3bbd7836561
Lucas <lucas.chen@ajeel.fr>
2022-03-22 14:47:12 +0100

    Updated new models

--- src/models/products.js
+++ src/models/products.js
@@ -13,6 +13,15 @@ const ProductSchema = new mongoose.Schema({
       type: String,
       required: true
   },
+  price: {
+      type: Number,
+      required: true
+  },
+  desc: {
+      type: String,
+      required: true
+  }
+
 });
 
 const Product = mongoose.model("Product", ProductSchema);
