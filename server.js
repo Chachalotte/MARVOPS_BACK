d069799b3aaeb42ddf0baa00ae9a6068b11f2891
Lucas <lucas.chen@ajeel.fr>
2022-03-23 09:24:44 +0100

    Update of socket for chat

--- server.js
+++ server.js
@@ -42,11 +42,13 @@ let io = socket(server)
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-  });
-  
-  io.on('connection', (socket) => {
-    console.log('a user connected');
-  });
+});
+
+io.on('connection', (socket) => {
+socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+});
+});
   
 
 server.on('error', errorHandler);
